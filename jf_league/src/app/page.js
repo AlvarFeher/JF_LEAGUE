53cacef463ade25877007c51d4b57f30a3b8d2cd
alvarofeher <alvarofeherargullos@gmail.com>
2025-05-30 14:00:26 +0200

    home view button alligned

--- jf_league/src/app/page.js
+++ jf_league/src/app/page.js
@@ -4,27 +4,28 @@ export default function Home() {
   return (
     <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
       <main className="flex flex-col items-center justify-center h-full gap-8">
-        <h1 className="text-3xl font-bold">Welcome to JF League</h1>
-        <ul>
+        <h1 className="text-3xl font-bold text-center">Welcome to JF League</h1>
+
+        <div className="flex flex-col gap-4 w-full max-w-xs">
           <a
             href="/select_players"
-            className="bg-black text-white px-6 py-3 rounded hover:bg-gray-800 transition"
+            className="bg-black text-white text-center px-6 py-3 rounded hover:bg-gray-800 transition"
           >
             Start Match
           </a>
           <a
             href="/stats"
-            className="bg-black text-white px-6 py-3 rounded hover:bg-gray-800 transition"
+            className="bg-black text-white text-center px-6 py-3 rounded hover:bg-gray-800 transition"
           >
             View Stats
           </a>
-        </ul>
-
+        </div>
       </main>
 
-      <footer className="row-start-3 flex gap-[24px] flex-wrap items-center justify-center">
-        <p>Web oficial de gestió de jornades de la <b>JF League</b></p>
+      <footer className="row-start-3 text-center text-sm text-gray-600">
+        Web oficial de gestió de jornades de la <b>JF League</b>
       </footer>
     </div>
   );
 }
+
